b5a89d7e56dac058396cbd417c1c0abedc5f5aac
Jordan Tucker <jordanbtucker@gmail.com>
2019-03-05 08:55:09 +0000

    Remove unused var in test

--- test/app.js
+++ test/app.js
@@ -6,7 +6,7 @@ const App = require('../lib/app')
 
 tap.test('App', async t => {
 	await t.test('run', async t => {
-		await t.test(`logs 'Hello, $NAME!'`, async t => {
+		await t.test(`logs 'Hello, $NAME!'`, async () => {
 			const message = `Hello, ${process.env.NAME}!`
 
 			const mock = sinon.mock(console)
