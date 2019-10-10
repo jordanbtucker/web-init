daf33a6c3d20fb7ca943089d14714c79f66de145
Jordan Tucker <jordanbtucker@gmail.com>
2019-10-10 16:24:36 -0500

    fix: avoid sub-apps

--- test/server/api.js
+++ test/server/api.js
@@ -2,23 +2,22 @@ require('dotenv/config')
 const t = require('tap')
 const supertest = require('supertest')
 const config = require('../../lib/server/config')
-const api = require('../../lib/server/api')
+const app = require('../../lib/server/app')
 
 const baseURL = `http://${config.host}:${config.port}`
 
 t.test('api', async t => {
   t.beforeEach(done => {
-    t.server = api.listen(config.port, config.host, done)
-    api.setup(t.server)
+    t.server = app.listen(config.port, config.host, done)
   })
 
   t.afterEach(async () => {
     t.server.close()
   })
 
-  await t.test('200 /messages', async () => {
+  await t.test('200 /api/messages', async () => {
     await supertest(baseURL)
-      .get('/messages')
+      .get('/api/messages')
       .expect(200)
   })
 })
